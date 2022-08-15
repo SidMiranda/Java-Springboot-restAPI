6d8022e1f658abe8440f246b2c12261ac47b04fa
Sidney Miranda <41812064+SidMiranda@users.noreply.github.com>
2022-08-15 18:41:35 -0300

    first commit

---
+++ src/main/java/com/example/teste/demo/ClientController.java
@@ -0,0 +1,31 @@
+package com.example.teste.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClientController {
+
+    @Autowired
+    private ClienteRepository cr;
+    
+    @GetMapping
+    public List<Cliente> listar(){
+        return cr.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente add(@RequestBody Cliente cliente){
+        return cr.save(cliente);
+    }
+}
